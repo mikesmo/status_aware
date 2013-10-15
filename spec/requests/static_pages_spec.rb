63942a2870b4cfb5472bdf8ae6934ba8c4a5c1ca
Michael <mike_smo@yahoo.com>
2013-10-15 13:35:30 +1100

    Simple test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe 'home page' do
-  it 'should log user in', :js => true do
+  it 'should log user in' do
     visit '/'
+
+    #fill_in 'Email or Phone', :with => 'michael.smolenski.junk@gmail.com'
+    #fill_in 'Password', :with => 'Trustme8'
+    #click_button "Log In"
+
     page.should have_content('Facebook')
   end
 end
