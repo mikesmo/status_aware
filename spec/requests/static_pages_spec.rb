01d992cfdbb729843f562eb31c60fd5e08c0ae05
Michael <mike_smo@yahoo.com>
2013-10-15 13:00:47 +1100

    change gem environment for capybara etc

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'home page' do
-  it 'should log user in', :js => true do
+  it 'should log user in' do
     visit '/'
     page.should have_content('Facebook')
   end
