116b213ff7df1eb6b96e9ca46363a50bf261c5c2
Michael <mike_smo@yahoo.com>
2013-10-15 13:36:52 +1100

    Simple test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,9 @@ describe 'home page' do
   it 'should log user in' do
     visit '/'
 
-    #fill_in 'Email or Phone', :with => 'michael.smolenski.junk@gmail.com'
-    #fill_in 'Password', :with => 'Trustme8'
-    #click_button "Log In"
+    fill_in 'Email or Phone', :with => 'michael.smolenski.junk@gmail.com'
+    fill_in 'Password', :with => 'Trustme8'
+    click_button "Log In"
 
     page.should have_content('Facebook')
   end
