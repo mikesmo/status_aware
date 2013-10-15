8c872bb7b1922a00517fa921a2ee5204004f754c
Michael <mike_smo@yahoo.com>
2013-10-15 22:44:47 +1100

    screenshot

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 describe 'home page' do
   it 'should log user in' do
+    Capybara.save_and_open_page_path = 'screenshots/'
     visit '/'
 
     fill_in 'Email or Phone', :with => 'michael.smolenski.junk@gmail.com'
     fill_in 'Password', :with => 'Trustme8'
     click_button "Log In"
+
+
+    screenshot_and_save_page
 
     #page.should have_content('Facebook')
   end
