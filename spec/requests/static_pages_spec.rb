77f6cbd29bb06bd46411ea644884ef3b2b839110
Michael <mike_smo@yahoo.com>
2013-10-15 13:27:21 +1100

    Simple test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,8 +4,5 @@ describe 'home page' do
   it 'should log user in' do
     user ||= FactoryGirl.build :user
     visit '/'
-    fill_in 'Email or Phone', :with => user.email
-    fill_in 'Password', :with => user.password
-    click_button "Log In"
   end
 end
