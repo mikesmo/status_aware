fca0a8a744beadb5f0d4660cd364a9841d090f55
Michael <mike_smo@yahoo.com>
2013-10-15 13:20:04 +1100

    Simple test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'home page' do
-  it 'should log user in', :js => true do
+  it 'should log user in' do
     user ||= FactoryGirl.build :user
     visit '/'
     fill_in 'Email or Phone', :with => user.email
